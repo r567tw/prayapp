ebbe033476e549a166d0d487b2cc114672d5f39d
r567tw <r567tw@gmail.com>
2015-08-20 09:34:52 +0800

    修正少許Bug

--- pray.py
+++ pray.py
@@ -4,14 +4,14 @@
 
 #讀取檔案
 def readfile(name):
-    file = open(name, 'r', encoding = 'UTF-8')
+    file = open(name, 'r',)
     content=file.read()
-    return content
+    print(content)
     file.close()
 
 #寫取檔案
 def writefile(name,content):
-    file = open(name, 'a+', encoding = 'UTF-8')
+    file = open(name, 'a+',)
     file.write(content)
     print('檔案寫入成功')
     file.close()
@@ -47,12 +47,10 @@ while (play != '0' ):
             content=content+input('請輸入你的被禱告人或者禱告者:')+','
             content=content+input('請輸入你的應許日期(若沒有則輸入N):')+'\n'
             writefile(data,content)
-            value=readfile(data)
             add=input("是否要繼續輸入下一筆?(若結束則輸入0)：")
         play=input("是否要離開程式?(若結束則輸入0)：")
     elif (access=='2'):
         value=readfile(data)
-        print(value)
         play=input("是否要離開程式?(若結束則輸入0)：")
     #小小彩蛋-宣告我的名字就可以結束程式~~
     elif (access=="I'm Jimmy fang"):
@@ -60,7 +58,6 @@ while (play != '0' ):
     else:
         print('輸入錯誤，請重新輸入ㄧ次')
 quit()
-
 
 
 
